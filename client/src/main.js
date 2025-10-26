9fd39a36768440b791cf8f3c657d97861e187e8e
Uffe Arlo <uat@aperitas.com>
2025-10-26 10:07:05 +0100

    Experiment with js import from public.

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,10 @@
 /* Activate Tailwind */
 import "./main.css";
+import { Module } from "./tools/module.js";
 
 const image = document.createElement('img');
 image.src = '/images/sprocket.webp'
 document.body.append(image)
+
+const {foo} = await Module.import('/test/foo.js')
+console.log('foo:', foo)
---
+++ client/src/tools/module.js
@@ -0,0 +1,23 @@
+export const Module = new (class {
+  #_ = {
+    import_: Function("url", "return import(url)"),
+  };
+  async create(text, path) {
+    if (path) {
+      text = `${text}\n//# sourceURL=${path}`;
+    }
+    const url = URL.createObjectURL(
+      new Blob([text], {
+        type: "text/javascript",
+      })
+    );
+    const result = await this.#_.import_(url);
+    URL.revokeObjectURL(url);
+    return result
+  }
+
+  /* Imports module from url. */
+  async import(url) {
+    return await this.#_.import_(url);
+  }
+})();
