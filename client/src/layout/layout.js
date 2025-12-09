fcf51561a319e3ad7e64c3547492790dcb4a4e6c
Uffe Arlo <uat@aperitas.com>
2025-12-09 10:43:55 +0100

    k

--- client/src/layout/layout.js
+++ client/src/layout/layout.js
@@ -2,7 +2,7 @@ import "@/use.js";
 import { app } from "@/app/app.js";
 import { Mixins, author, component, mix } from "@/component/component.js";
 //
-import "./assets/layout.css";////
+
 //await use("/assets/layout/layout.css");
 
 const reboot = await use("@/bootstrap/reboot.css");
