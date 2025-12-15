678a14c4768e7ad7c29f37a4fda14108abdd14b3
Uffe Arlo <uat@aperitas.com>
2025-12-15 11:07:31 +0100

    k

--- client/api/ping.py
+++ client/api/ping.py
@@ -1,3 +1,4 @@
+import json
 from http.server import BaseHTTPRequestHandler
 
 
@@ -5,12 +6,14 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
 
         result = dict(ping=42)
+        result = json.dumps(result).encode("utf-8")
 
         self.send_response(200)
-        self.send_header("content-type", "application/json")
+        self.send_header("content-type", "application/json; charset=utf-8")
+        self.send_header("content-length", str(len(result)))
         self.end_headers()
 
-        self.wfile.write(result.encode("utf-8"))
+        self.wfile.write(result)
 
 
 # curl https://rolloh.vercel.app/api/ping
