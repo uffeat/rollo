06d7e9989a3e0b24fd21594c19d8f28149a510c6
Uffe Arlo <uat@aperitas.com>
2025-12-15 11:09:13 +0100

    k

--- client/api/echo.py
+++ client/api/echo.py
@@ -11,10 +11,12 @@ class handler(BaseHTTPRequestHandler):
         connect(key)
 
         result = call("echo", data=dict(foo=42))
+        result = json.dumps(result).encode("utf-8")
        
 
         self.send_response(200)
-        self.send_header("content-type", "application/json")
+        self.send_header("content-type", "application/json; charset=utf-8")
+        self.send_header("content-length", str(len(result)))
         self.end_headers()
 
-        self.wfile.write(result.encode("utf-8"))
+        self.wfile.write(result)
