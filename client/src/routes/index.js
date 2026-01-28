5094feb86c1f18285b6a6e28810e3fa9833a429c
Uffe Arlo <uat@aperitas.com>
2026-01-28 13:10:09 -0500

    q

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,13 +1,14 @@
-import "@/use";
+/* NOTE Typically, the 'routes' parcel contains all routes and simply running 
+'setup' suffices. However, since parcels are not Reat-aware, if a route uses React,
+routes should be progressively enhanced at app point. This shows how to do that. */ 
 
-import './routes/articles'
+import "@/use";
+import "./routes/articles";
 
 const { component } = await use("@/rollo/");
 const { Nav, NavLink, router, Route } = await use("@/router/");
 const { frame } = await use("@/frame/");
 const { nav, setup } = await use("@/routes/");
-
-
 
 /* Complete router setup */
 await setup();
