9b4654975ace01c8c96e8292ca6278958a829f62
Uffe Arlo <uat@aperitas.com>
2026-02-19 16:10:10 +0100

    k

--- client/src/main.js
+++ client/src/main.js
@@ -4,17 +4,28 @@ import "@/use";
 /* Initialize iworker */
 //import { iworker } from "@/iworker";
 /* Set up routes */
-//import "@/routes";
+import "@/routes";
 
 const { app, component } = await use("@/rollo/");
 
-const iframe = component.iframe({src: 'https://rollohdev.anvil.app'})
+const iframe = component.iframe({ 
+  src: "https://rollohdev.anvil.app?foo=42", 
+  //slot: 'data', 
+},
+);
 
+await new Promise((resolve, reject) => {
+  iframe.on.load({ once: true }, (event) => {
+    resolve(iframe.contentWindow);
+  });
 
-app.append(iframe)
+  app.append(iframe);
+});
+
+console.log("contentWindow:", iframe.contentWindow);
 
 if (import.meta.env.DEV) {
-  await use('/parcels/main/main.css')
+  await use("/parcels/main/main.css");
   /* Initialize DEV testbench */
   await import("../test");
 }
