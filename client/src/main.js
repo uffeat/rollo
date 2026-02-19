9bced40c45f5a8f929c7a3b58938b6013fbab7dc
Uffe Arlo <uat@aperitas.com>
2026-02-19 19:38:17 +0100

    k

--- client/src/main.js
+++ client/src/main.js
@@ -8,32 +8,74 @@ import "@/routes";
 
 if (import.meta.env.DEV) {
   await use("/parcels/main/main.css");
- 
 }
 
 const { app, component } = await use("@/rollo/");
 const { frame } = await use("@/frame/");
 
-const iframe = component.iframe( {
-  name: 'iworker',
-  src: `${use.meta.server.origin}?origin=${origin}`,
-  $undisplay: true
-  
+const iframe = component.iframe({
+  name: "iworker",
+  src: `${use.meta.server.origin}?iworker=`,
+
+  $stateHidden: true,
 });
 
 await new Promise((resolve, reject) => {
   iframe.on.load({ once: true }, (event) => {
     resolve(iframe.contentWindow);
   });
-
   app.append(iframe);
 });
 
-console.log("contentWindow:", iframe.contentWindow);
+await new Promise((resolve, reject) => {
+  const onmessage = (event) => {
+    if (event.origin !== use.meta.server.origin) {
+      return;
+    }
+    if (event.data === "ready") {
+      window.removeEventListener("message", onmessage);
+      resolve(true);
+    }
+  };
 
-const show = component.button('btn.btn-primary.m-3', {parent: frame, 'on.click': (event) => {
-  iframe.$.undisplay = false
-}}, 'Show iworker')
+  window.addEventListener("message", onmessage);
+});
+
+console.log("contentWindow:", iframe.contentWindow); ////
+
+const show = component.button(
+  "btn.btn-primary.m-3",
+  {
+    parent: frame,
+    "on.click": (event) => {
+      iframe.$.stateHidden = false;
+    },
+  },
+  "Show iworker",
+);
+
+const request = async (specifier, ...args) => {
+  return new Promise((resolve, reject) => {
+    const channel = new MessageChannel();
+    channel.port1.onmessage = (event) => {
+      if (event.data.error) {
+        reject(event.data.error);
+      } else {
+        resolve(event.data.result);
+      }
+      channel.port1.close();
+    };
+    iframe.contentWindow.postMessage(
+      { type: "request", specifier, args },
+      use.meta.server.origin,
+      [channel.port2],
+    );
+  });
+};
+
+request("echo", 42).then((result) => {
+  console.log("result:", result);
+});
 
 if (import.meta.env.DEV) {
   /* Initialize DEV testbench */
