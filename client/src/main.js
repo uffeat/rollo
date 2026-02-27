2cc79cfc5e383793198be8bedf3b1da024334660
Uffe Arlo <uat@aperitas.com>
2026-02-27 19:10:12 +0100

    k

--- client/src/main.js
+++ client/src/main.js
@@ -8,7 +8,7 @@ import "@/routes";
 
 if (import.meta.env.DEV) {
   //await use("/parcels/main/main.css");
-  await import('@/dev.css')
+  await import("@/dev.css");
 }
 
 //const { user } = await use("@/user/");
@@ -521,6 +521,22 @@ const iframe2 = component.iframe({
   parent: app
 });
 */
+
+/* */
+await (async () => {
+  const response = await fetch(
+    `https://fastapilab.onrender.com/_/api/main?name=echo&submission=0`,
+    {
+      //mode: "cors",
+      method: "POST",
+      headers: { "Content-Type": "text/plain" },
+      body: JSON.stringify({ args: [1, 2, 3], kwargs: { stuff: 42 } }),
+    },
+  );
+  console.log("response:", response); ////
+  const result = await response.json();
+  console.log("result:", result); ////
+})();
 
 if (import.meta.env.DEV) {
   /* Initialize DEV testbench */
