a39da9d21562de9fd10f786c1911ae67a30baa33
Uffe Arlo <uat@aperitas.com>
2026-03-29 23:29:02 +0200

    q

--- parcels/iworker/test/tests/dev.test.js
+++ parcels/iworker/test/tests/dev.test.js
@@ -12,27 +12,42 @@ use.sources.add("@@", async ({ options, owner, path }, ...args) => {
   if (options.visible) {
     return await iworker.show(path.specifier, options, ...args);
   }
-
   return await iworker.request(path.specifier, options, ...args);
+});
 
- 
+use.sources.add("rpc", async ({ options, owner, path }, ...args) => {
+  return await iworker.request(path.specifier, options, ...args);
+});
+
+use.sources.add("api", async ({ options, owner, path }, ...args) => {
+  return await iworker.request(path.specifier, options, ...args);
 });
 
 export default async () => {
   await (async () => {
-    const result = await use("@@/echo/",  { foo: 42 }, 1, 2, 3);
+    const result = await use("@@/echo/", { foo: 42 }, 1, 2, 3);
     console.log("@@/echo/ result:", result); ////
   })();
 
   await (async () => {
-    const result = await use("@@/foo/", {visible: true});
+    const result = await use("rpc/echo/", {}, { foo: 42 }, 1, 2, 3);
+    console.log("rpc/echo/ result:", result); ////
+  })();
+
+  await (async () => {
+    const result = await use("api/echo/", {}, { foo: 42 }, 1, 2, 3);
+    console.log("api/echo/ result:", result); ////
+  })();
+
+  
+
+  await (async () => {
+    const result = await use("@@/foo/", { visible: true });
     console.log("@@/foo/ result:", result); ////
   })();
 
   await (async () => {
-    const result = await use("@@/stuff/", {visible: 'popover'});
+    const result = await use("@@/stuff/", { visible: "popover" });
     console.log("@@/foo/ result:", result); ////
   })();
-
-  
 };
