49adecbc828f380842fef7be8735f2eb7b43af7b
Uffe Arlo <uat@aperitas.com>
2026-04-06 21:32:44 +0200

    k

--- client/src/main.js
+++ client/src/main.js
@@ -8,9 +8,32 @@ if (import.meta.env.DEV) {
   await import("@/dev.css");
 }
 
-await use('@/iworker/');
+await use("@/iworker/");
 
+await (async () => {
+    const result = await use(
+      "rpc/echo/",
+      { test: true },
+      { foo: "FOO", things: [{ a: 1 }, { b: 2 }] },
+      { first: "FIRST" },
+      10,
+      20,
+      30,
+      { last: "LAST" },
+    );
+    console.log("rpc/echo/ result:", result);
+  })();
 
+await (async () => {
+  const result = await use(
+    "@@/login/",
+    {
+      visible: "popover",
+      test: true,
+    },
+  );
+  console.log("login result:", result);
+})();
 
 if (import.meta.env.DEV) {
   /* Initialize DEV testbench */
--- test/client_code/tests/use/own/foo.py
+++ test/client_code/tests/use/own/foo.py
@@ -5,10 +5,7 @@ use/own/foo.py
 
 def main(use, *args, **kwargs):
     """."""
-    import json
-
-    assets = use.assets
-    component_from = use("@/rollo/").htmlToComponent
+    console = use.console
     document = use.document
     JSON = use("@@/js").JSON
     log = use.log
@@ -16,9 +13,13 @@ def main(use, *args, **kwargs):
     Promise = use("@@/promise").Promise
     source = use.source
     works = use.anvil
+    js = use("@@/js/")
+
+    _assets = use.assets
+    Sheet = _assets.get("@/rollo/").Sheet
 
     @source()
-    class Assets:
+    class assets:
 
         cache = {}
 
@@ -29,9 +30,27 @@ def main(use, *args, **kwargs):
                 # Escape subsequent transpilation/processing
                 path.detail.escape = True
                 if options.get("raw"):
-                    return self.get_text(path)
+                    return self.get_text(path, test=test)
+
+                # HACK
+                if meta.DEV and test:
+                    try:
+                        style = document.head.querySelector(
+                            f'style[path="{path.path}"]'
+                        )
+                        if style:
+                            return style
+                        text = works.server.call("_assets", path.path)
+                        style = document.createElement("style")
+                        style.textContent = text
+                        style.setAttribute("path", path.path)
+                        document.head.append(style)
+                        return style
+                    except Exception as error:
+                        console.warn(f"Not using injected {path.path}. {str(error)}")
+
                 # Add or get link
-                href = f"{meta.origin}/_/theme{path.path}"
+                href = f"/_/theme{path.path}"
                 link = document.head.querySelector(f'link[href="{href}"]')
                 if not link:
                     link = document.createElement("link")
@@ -49,61 +68,78 @@ def main(use, *args, **kwargs):
                 if path.types == "css.html":
                     # Ensure subsequent treatment as pure type
                     path.type = path.types = "css"
-                    node = self.create_node(path)
-                    style = node.querySelector("style[sheet]")
-                    text = style.textContent.strip()
-                    return text
+                    node = self.create_node(path, test=test)
+                    selector = "style[sheet]"
+                    style = node.querySelector(selector)
+                    if not style:
+                        raise ValueError(
+                            f"No result from selector '{selector}' ({path.path})."
+                        )
+                    result = style.textContent.strip()
+                    return result
 
                 if path.types == "html.html":
                     # Ensure subsequent treatment as pure type
                     path.type = path.types = "html"
-                    node = self.create_node(path)
-                    return node.innerHTML.strip()
+                    node = self.create_node(path, test=test)
+                    result = node.innerHTML.strip()
+                    if not result:
+                        raise KeyError(f"Invalid asset: {path.path}.")
+                    return result
+
+                if path.types == "jinja.html":
+                    # Ensure subsequent treatment as html
+                    path.type = path.types = "html"
+                    node = self.create_node(path, test=test)
+                    result = node.innerHTML.strip()
+                    if not result:
+                        raise KeyError(f"Invalid asset: {path.path}.")
+                    return self.render(result, **options.get("data", {}))
 
                 if path.types == "js.html":
                     # Ensure subsequent treatment as pure type
                     path.type = path.types = "js"
-                    node = self.create_node(path)
-                    template = node.querySelector("template[module]")
+                    selector = "template[module]"
+                    node = self.create_node(path, test=test)
+                    template = node.querySelector(selector)
+                    if not template:
+                        raise ValueError(
+                            f"No result from selector '{selector}' ({path.path})."
+                        )
                     script = template.content.querySelector("script")
+                    if not script:
+                        raise ValueError(f"No script in '{selector}' ({path.path}).")
                     text = script.textContent.strip()
                     return text
 
                 if path.types == "svg.html":
                     # Ensure subsequent treatment as pure type
                     path.type = path.types = "svg"
-                    node = self.create_node(path)
-                    return node.innerHTML.strip()
-
-                ##
-                ##
-                if path.types == "txt.html":
-                    # Escape subsequent transpilation/processing
-                    path.detail.escape = True
-                    node = self.create_node(path)
-                    pre = node.querySelector("pre")
-                    text = pre.textContent.strip()
-                    return text
-                ##
-                ##
+                    node = self.create_node(path, test=test)
+                    result = node.innerHTML.strip()
+                    if not result:
+                        raise KeyError(f"Invalid asset: {path.path}.")
+                    return result
 
                 # Default -> text
-                return self.get_text(path)
+                return self.get_text(path, test=test)
 
-            
             if path.type == "jinja":
+                # Ensure subsequent treatment as html
                 path.type = path.types = "html"
-                text = self.get_text(path)
-                content = text.replace('{{', '{').replace('}}', '}')
-                rich = works.RichText(enable_slots=True, format="plain_text", content=content)
-                rich.data = options.get('data', {})
-                result = works.get_dom_node(rich).textContent
-                return result
-            
-            
+                text = self.get_text(path, test=test)
+                return self.render(text, **options.get("data", {}))
+
             if path.type == "js":
                 if options.get("raw"):
-                    return self.get_text(path)
+                    return self.get_text(path, test=test)
+                if meta.DEV and test:
+                    try:
+                        return js.module(
+                            works.server.call("_assets", path.path), path=path.path
+                        )
+                    except Exception as error:
+                        console.warn(f"Not using injected {path.path}. {str(error)}")
                 return works.js.import_from(f"/_/theme{path.path}")
 
             if path.type == "md":
@@ -111,84 +147,102 @@ def main(use, *args, **kwargs):
                 path.detail.escape = True
                 if path.path in self.cache:
                     return self.cache[path.path]
-                content = self.get_text(path)
+                content = self.get_text(path, test=test)
                 rich = works.RichText(format="markdown", content=content)
                 result = works.get_dom_node(rich).innerHTML
                 self.cache[path.path] = result
                 return result
 
             # Default -> text
-            return self.get_text(path)
+            return self.get_text(path, test=test)
 
         @staticmethod
-        def create_node(path):
-            node = works.js.get_dom_node(
+        def create_node(path, test=False):
+            if meta.DEV and test:
+                try:
+                    html = works.server.call("_assets", path.path)
+                    node = document.createElement("div")
+                    node.innerHTML = html
+                    return node
+                except Exception as error:
+                    console.warn(f"Not using injected {path.path}. {str(error)}")
+            return works.js.get_dom_node(
                 works.HtmlTemplate(html=f"@theme:{path.path[1:]}")
             )
-            return node
 
         @staticmethod
-        def get_text(path):
-            text = (
-                works.URLMedia(f"{meta.origin}/_/theme{path.path}")
-                .get_bytes()
-                .decode("utf-8")
+        def render(text, **data):
+            """."""
+            content = text.replace("{{", "{").replace("}}", "}")
+            rich = works.RichText(
+                enable_slots=True, format="plain_text", content=content
             )
+            rich.data = data
+            result = works.get_dom_node(rich).textContent
+            return result
+
+        def get_text(self, path, cache=False, test=False):
+            if meta.DEV and test:
+                try:
+                    return works.server.call("_assets", path.path)
+                except Exception as error:
+                    console.warn(f"Not using injected {path.path}. {str(error)}")
+            if cache and path.path in self.cache:
+                return self.cache[path.path]
+            text = works.URLMedia(f"/_/theme{path.path}").get_bytes().decode("utf-8")
+            if cache:
+                self.cache[path.path] = text
             return text
 
+    # ** Test
+    import json
+
+    from_html = use("@@/component").from_html
+    FOO = "assets/tests/foo"
+
     # Native pure type imports
-    log("link:", use("assets/tests/foo.css"), native=True)  ##
-    log("foo.css:", use("assets/tests/foo.css"), native=True)  # Repeat to check dedupe
-    log("foo.css raw:", use("assets/tests/foo.css", raw=True), native=True)
-    log("foo.js:", use("assets/tests/foo.js"), native=True)  ##
-    log("foo.js raw:", use("assets/tests/foo.js", raw=True), native=True)  ##
-    log("foo.json:", use("assets/tests/foo.json"), native=True)  ##
-    log("foo.json raw:", use("assets/tests/foo.json", raw=True), native=True)  ##
-    log("foo.json py:", json.loads(use("assets/tests/foo.json", raw=True)))  ##
+    log("Link:", use(f"{FOO}.css", test=True), native=True)
+    log("Link:", use(f"{FOO}.css", test=True), native=True)  # Repeat to check dedupe
+    log("Raw css:", use(f"{FOO}.css", raw=True, test=True), native=True)
+    log("foo member:", use(f"{FOO}.js", test=True).foo, native=True)
+    log("Raw module:", use(f"{FOO}.js", raw=True, test=True), native=True)
+    log("From json:", use(f"{FOO}.json", test=True), native=True)
+    log("Raw json:", use(f"{FOO}.json", raw=True, test=True), native=True)
+    log("From json, py:", json.loads(use(f"{FOO}.json", raw=True, test=True)))
 
     # Import of composite html types
-    log("foo.css.html:", use("assets/tests/foo.css.html"), native=True)  ##
-    log("foo.html.html:", use("assets/tests/foo.html.html"), native=True)  ##
-    log("foo.js.html:", use("assets/tests/foo.js.html"), native=True)  ##
-    log("foo.svg.html:", use("assets/tests/foo.svg.html"), native=True)  ##
-
-    ##
-    log("foo.txt.html:", use("assets/tests/foo.txt.html"), native=True)  ##
-    ##
+    log("Sheet:", use(f"{FOO}.css.html", test=True), native=True)
+    log("Markup:", use(f"{FOO}.html.html", test=True), native=True)
+    log("Module:", use(f"{FOO}.js.html", test=True), native=True)
+    log("Icon:", use(f"{FOO}.svg.html", test=True), native=True)
 
     # Markdown import
-    log("foo.md:", use("assets/tests/foo.md"), native=True)  ##
+    log("From markdown:", use(f"{FOO}.md", test=True), native=True)
 
     # Component import from html
     log(
         "component:",
-        use("assets/tests/foo.html", convert=True),
+        use(f"{FOO}.html", convert=True),
         native=True,
-    )  ##
+    )
 
-    # Lab
-    def render(path, **data):
-        content = use(f"assets/{path}").replace('{{', '{').replace('}}', '}')
-        rich = works.RichText(enable_slots=True, format="plain_text", content=content)
-        rich.data = data
-        result = works.get_dom_node(rich).textContent
-        return result
+    # Render from jinja import
+    log(
+        "Rendered:",
+        use(f"{FOO}.jinja.html", data=dict(year=1969), test=True),
+        native=True,
+    )
+    log(
+        "Rendered and converted:",
+        use(f"{FOO}.jinja", data=dict(year=1969), convert=True, test=True),
+        native=True,
+    )
+    log(
+        "Rendered and wrapped:",
+        from_html(
+            "section.foo.bar", use(f"{FOO}.jinja", data=dict(year=1969), test=True)
+        ),
+        native=True,
+    )
+
     
-    print("rendered:", render('tests/foo.jinja', year=1969, stuff=42))
-    
-
-
-
-
-    text = use("assets/tests/foo.jinja")
-    text = text.replace('{{', '{').replace('}}', '}')
-    rich = works.RichText(enable_slots=True, format="plain_text", content=text)
-    rich.data = dict(year=1969)
-    result = works.get_dom_node(rich).textContent
-    print("result:", result)
-
-    print("component:", component_from('<h1>Hi</h1>'))
-
-
-    log("foo.jinja:", use("assets/tests/foo.jinja", data=dict(year=1969)), native=True)  ##
-    log("foo.jinja converted:", use("assets/tests/foo.jinja", data=dict(year=1969), convert=True), native=True)  ##
